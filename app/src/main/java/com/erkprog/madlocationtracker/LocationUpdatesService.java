7b185b6d5567f7d165b9a3c5e44bd3afeca6de5b
erlanamanatov <erlanamanatov@gmail.com>
2019-01-09 01:24:25 +0600

    Create basic mainactivity functionality

--- app/src/main/java/com/erkprog/madlocationtracker/LocationUpdatesService.java
+++ app/src/main/java/com/erkprog/madlocationtracker/LocationUpdatesService.java
@@ -2,13 +2,29 @@ package com.erkprog.madlocationtracker;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class LocationUpdatesService extends Service {
+
+  private static final String TAG = LocationUpdatesService.class.getSimpleName();
 
   @androidx.annotation.Nullable
   @Override
   public IBinder onBind(Intent intent) {
     return null;
+  }
+
+  public void requestLocationUpdates() {
+  }
+
+  public void removeLocationUpdates() {
+  }
+
+
+  public class LocalBinder extends Binder {
+    LocationUpdatesService getService() {
+      return LocationUpdatesService.this;
+    }
   }
 }
--- app/src/main/java/com/erkprog/madlocationtracker/MainActivity.java
+++ app/src/main/java/com/erkprog/madlocationtracker/MainActivity.java
@@ -1,11 +1,35 @@
 package com.erkprog.madlocationtracker;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+  private LocationUpdatesService mService = null;
+
+  private boolean mBound = false;
+
+  private final ServiceConnection mServiceConnection = new ServiceConnection() {
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+      LocationUpdatesService.LocalBinder binder = (LocationUpdatesService.LocalBinder) service;
+      mService = binder.getService();
+      mBound = true;
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+      mService = null;
+      mBound = false;
+    }
+  };
 
   Button buttonRequestLocationUpdates, buttonRemoveLocationUpdates;
 
@@ -17,22 +41,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
   }
 
   private void init() {
-    buttonRequestLocationUpdates= findViewById(R.id.button_request_location);
+    buttonRequestLocationUpdates = findViewById(R.id.button_request_location);
     buttonRequestLocationUpdates.setOnClickListener(this);
     buttonRemoveLocationUpdates = findViewById(R.id.button_remove_locations);
     buttonRemoveLocationUpdates.setOnClickListener(this);
+  }
 
+  @Override
+  protected void onStart() {
+    super.onStart();
+    bindService(new Intent(this, LocationUpdatesService.class), mServiceConnection,
+        Context.BIND_AUTO_CREATE);
+  }
+
+  @Override
+  protected void onStop() {
+    if (mBound) {
+      unbindService(mServiceConnection);
+      mBound = false;
+    }
+    super.onStop();
   }
 
   @Override
   public void onClick(View v) {
     switch (v.getId()) {
       case R.id.button_request_location:
-        //TODO: on request location updates
+        mService.requestLocationUpdates();
         break;
 
       case R.id.button_remove_locations:
-        //TODO: on remove location updates
+        mService.removeLocationUpdates();
         break;
     }
   }
