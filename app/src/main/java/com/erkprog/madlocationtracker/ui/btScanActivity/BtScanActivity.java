f31466febf7c22e5f3d334cd0f65d48cc28ea01a
erlanamanatov <erlanamanatov@gmail.com>
2019-03-10 23:28:03 +0600

    Add progress bar for bluetoothLE scanning indication

--- app/src/main/java/com/erkprog/madlocationtracker/ui/btScanActivity/BtScanActivity.java
+++ app/src/main/java/com/erkprog/madlocationtracker/ui/btScanActivity/BtScanActivity.java
@@ -10,8 +10,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.erkprog.madlocationtracker.R;
+
+import static android.view.View.GONE;
 
 public class BtScanActivity extends AppCompatActivity implements BtDevicesAdapter.OnDeviceClickListener {
   private static final String TAG = "BtScanActivity";
@@ -26,6 +31,8 @@ public class BtScanActivity extends AppCompatActivity implements BtDevicesAdapte
 
   private RecyclerView mRecyclerView;
   private BtDevicesAdapter mBtDevicesAdapter;
+  private ProgressBar mProgressBar;
+  private Button btRefresh;
 
   // Stops scanning after 10 seconds.
   private static final long SCAN_PERIOD = 10000;
@@ -39,6 +46,11 @@ public class BtScanActivity extends AppCompatActivity implements BtDevicesAdapte
     mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
     mBtDevicesAdapter = new BtDevicesAdapter(this);
     mRecyclerView.setAdapter(mBtDevicesAdapter);
+
+    mProgressBar = findViewById(R.id.bt_scan_prb);
+    mProgressBar.setVisibility(GONE);
+    btRefresh = findViewById(R.id.bt_scan_refresh);
+    btRefresh.setOnClickListener(v -> scanLeDevice(true));
 
     final BluetoothManager bluetoothManager =
         (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
@@ -59,6 +71,7 @@ public class BtScanActivity extends AppCompatActivity implements BtDevicesAdapte
       mHandler.postDelayed(() -> {
         mScanning = false;
         mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        setViewState(mScanning);
       }, SCAN_PERIOD);
 
       mScanning = true;
@@ -67,6 +80,7 @@ public class BtScanActivity extends AppCompatActivity implements BtDevicesAdapte
       mScanning = false;
       mBluetoothAdapter.stopLeScan(mLeScanCallback);
     }
+    setViewState(mScanning);
   }
 
   private BluetoothAdapter.LeScanCallback mLeScanCallback =
@@ -76,6 +90,16 @@ public class BtScanActivity extends AppCompatActivity implements BtDevicesAdapte
           mBtDevicesAdapter.notifyDataSetChanged();
         }
       });
+
+  private void setViewState(boolean isScanning) {
+    if (isScanning) {
+      mProgressBar.setVisibility(View.VISIBLE);
+      btRefresh.setVisibility(GONE);
+    } else {
+      mProgressBar.setVisibility(View.INVISIBLE);
+      btRefresh.setVisibility(View.VISIBLE);
+    }
+  }
 
   @Override
   protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -90,5 +114,14 @@ public class BtScanActivity extends AppCompatActivity implements BtDevicesAdapte
     intent.putExtra(EXTRA_DEVICE_ADDRESS, deviceAddress);
     setResult(RESULT_OK, intent);
     finish();
+  }
+
+  @Override
+  protected void onStop() {
+    if (mScanning) {
+      mBluetoothAdapter.stopLeScan(mLeScanCallback);
+      mHandler.removeCallbacksAndMessages(null);
+    }
+    super.onStop();
   }
 }
