094015897c1cc0a736ed0af3c4ed7f7c8d30adb4
erlanamanatov <erlanamanatov@gmail.com>
2019-03-22 16:55:46 +0600

    Add assertions to location service test

--- app/src/androidTest/java/com/erkprog/madlocationtracker/LocationServiceTest.java
+++ app/src/androidTest/java/com/erkprog/madlocationtracker/LocationServiceTest.java
@@ -17,6 +17,7 @@ import androidx.test.rule.ServiceTestRule;
 
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static org.hamcrest.Matchers.comparesEqualTo;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
@@ -24,37 +25,43 @@ import static org.junit.Assert.assertTrue;
 @RunWith(AndroidJUnit4.class)
 public class LocationServiceTest {
 
-
   @Rule
   public final ServiceTestRule mServiceRule = new ServiceTestRule();
 
   @Test
-  public void testWithBoundService() throws TimeoutException, InterruptedException {
-    // Create the service Intent.
+  public void testLocationService() throws TimeoutException, InterruptedException {
     Intent serviceIntent =
         new Intent(getApplicationContext(), LocationUpdatesService.class);
 
-//    // Data can be passed to the service via the Intent.
-//    serviceIntent.putExtra(LocalService.SEED_KEY, 42L);
-
-    // Bind the service and grab a reference to the binder.
     IBinder binder = mServiceRule.bindService(serviceIntent);
 
-    // Get the reference to the service, or you can call public methods on the binder directly.
     LocationUpdatesService service = ((LocationUpdatesService.LocalBinder) binder).getService();
 
     assertTrue(service.isServiceStarted());
+
+    // start tracking, new FitActivity created here
     service.startTracking();
+
     Thread.sleep(500);
+    Location location = getTestLocation();
+    // test with fake location
+    service.locationChanged(location);
+
+    // check locationList, size of list should be greater than 0
+    assertThat(service.getLocationsList().size(), greaterThan(0));
+    // check id of fit activity, if id > 0 activity has been created in DB
+    assertThat((int) service.getCurrentFitActivity().getId(), greaterThan(0));
+    service.stopTracking(5000L);
+
+    Thread.sleep(500);
+    service.onDestroy();
+    assertFalse(service.isServiceStarted());
+  }
+
+  private Location getTestLocation() {
     Location location = new Location("test Location");
     location.setLatitude(42.42);
     location.setLongitude(74.24);
-    service.locationChanged(location);
-    System.out.println("testingshit " + String.valueOf(FitActivity.STATUS_TRACKING));
-    String s = service.getCurrentFitActivity() != null ? "not null" : "null";
-    System.out.println("testingshit " + s);
-    service.stopTracking(100L);
-    service.onDestroy();
-    assertFalse(service.isServiceStarted());
+    return location;
   }
 }
